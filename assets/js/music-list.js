6dcdad69cad76e64eeac9804543ba2b336958b51
Dennis00725 <dennis00725@gmail.com>
2024-03-26 01:33:09 +0530

    assests added

---
+++ assets/js/music-list.js
@@ -0,0 +1,20 @@
+let allMusic = [
+  {
+    name: "another love",
+    artist: "another love",
+    img: "music-1",
+    src: "music-1",
+  },
+  {
+    name: "Night Changes",
+    artist: "Artist Name",
+    img: "music-2",
+    src: "music-2",
+  },
+  {
+    name: "photograph",
+    artist: "photograph",
+    img: "music-3",
+    src: "music-3",
+  },
+];
