feec78c3e25999190105979d43aecfdbce6ecf21
Dennis00725 <dennis00725@gmail.com>
2024-03-27 08:09:50 +0530

    6 music added

--- assets/js/music-list.js
+++ assets/js/music-list.js
@@ -16,5 +16,29 @@ let allMusic = [
     artist: "Ed Sheeran", 
     img: "music-3",
     src: "music-3"
-  }
+  },
+  {
+    name: "Attention",
+    artist: "Charlie Puth", 
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Past Lives",
+    artist: "sapientdream, Slushii", 
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "I Hate U, I Love U",
+    artist: "Gnash - ft. Olivia O'Brien", 
+    img: "music-6",
+    src: "music-6"
+  },
+
+
+
+
+
+
 ];
